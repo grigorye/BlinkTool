4422da7411055ed79a5bb95e7d9c223b60d5eee8
Grigory Entin <grigory.entin@gmail.com>
2020-09-29 02:29:53 +0200

    Added `init(email:, password:)`.

--- Sources/BlinkTool/Implementation/BlinkController+GlobalOptions.swift
+++ Sources/BlinkTool/Implementation/BlinkController+GlobalOptions.swift
@@ -5,8 +5,11 @@ extension BlinkController {
     init(globalOptions: GlobalOptions) {
         let email = globalOptions.email
         let password = globalOptions.password
+        self.init(email: email, password: password)
+    }
+    
+    init(email: String, password: String?) {
         let authenticationTokenStorage = defaultAuthenticationTokenStorage(email: email)
-        self.init(
-            email: email, password: password, authenticationTokenStorage: authenticationTokenStorage)
+        self.init(email: email, password: password, authenticationTokenStorage: authenticationTokenStorage)
     }
 }
