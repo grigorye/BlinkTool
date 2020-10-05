7da062f2518a8eab3ff53aab8de3fb6240e198ab
Grigory Entin <grigory.entin@gmail.com>
2020-10-05 02:03:53 +0200

    Added support for Linux.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 
 import PackageDescription
 
@@ -13,12 +13,14 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/grigorye/BlinkKit", .branch("master")),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.5"),
+        .package(url: "https://github.com/OpenCombine/OpenCombine.git", from: "0.10.1"),
     ],
     targets: [
         .target(
             name: "BlinkTool",
             dependencies: [
                 "BlinkKit",
+                .product(name: "OpenCombine", package: "OpenCombine", condition: .when(platforms: [.linux])),
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
             ]
         ),
