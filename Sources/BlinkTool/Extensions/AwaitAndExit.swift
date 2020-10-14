8ae2174fa337cbecc13cbe8b6ebe2ccd15caa97d
Grigory Entin <grigory.entin@gmail.com>
2020-10-15 01:22:15 +0200

    Made correct exit status reported on failure.

--- Sources/BlinkTool/Extensions/AwaitAndExit.swift
+++ Sources/BlinkTool/Extensions/AwaitAndExit.swift
@@ -6,14 +6,21 @@ import Glibc
 import Darwin
 #endif
 
-func await(block: (_ exit: @escaping () -> Void) -> Void) {
+func await(block: (_ exit: @escaping (ExitStatus) -> Void) -> Void) {
     let dispatchGroup = DispatchGroup()
     dispatchGroup.enter()
-    block {
+    var trackedExitStatus: ExitStatus?
+    block { (exitStatus) in
+        trackedExitStatus = exitStatus
         dispatchGroup.leave()
     }
     dispatchGroup.notify(queue: DispatchQueue.main) {
-        exit(EXIT_SUCCESS)
+        switch trackedExitStatus! {
+        case .success:
+            exit(EXIT_SUCCESS)
+        case .failure:
+            exit(EXIT_FAILURE)
+        }
     }
     dispatchMain()
 }
--- Sources/BlinkTool/Extensions/Publisher+AwaitingAndTracking.swift
+++ Sources/BlinkTool/Extensions/Publisher+AwaitingAndTracking.swift
@@ -4,15 +4,20 @@ import Combine
 import OpenCombine
 #endif
 
+enum ExitStatus {
+    case success, failure
+}
+
 extension Publisher where Output: Codable {
-    func awaitAndTrack(exit: @escaping () -> Void, cancellables: inout Set<AnyCancellable>) {
+    func awaitAndTrack(exit: @escaping (ExitStatus) -> Void, cancellables: inout Set<AnyCancellable>) {
         sink { (completion: Subscribers.Completion<Self.Failure>) in
             if case .failure(let error) = completion {
                 track(error)
+                exit(.failure)
             }
-            exit()
         } receiveValue: { (value: Output) in
             track(value)
+            exit(.success)
         }
         .store(in: &cancellables)
     }
