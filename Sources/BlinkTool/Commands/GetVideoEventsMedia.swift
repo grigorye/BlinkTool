f86d6b008ab6bbb3b930c35f198dee1e53489b79
Grigory Entin <grigory.entin@gmail.com>
2021-05-21 12:29:57 +0200

    Added support for --max-downloads/segregated queryMediaForDownload.

--- Sources/BlinkTool/Commands/GetVideoEventsMedia.swift
+++ Sources/BlinkTool/Commands/GetVideoEventsMedia.swift
@@ -22,6 +22,9 @@ struct GetVideoEventsMedia: BlinkCommand {
     @Option(help: "Page")
     private var page: Int?
     
+    @Option(help: "Maximum number of simultaneous downloads")
+    private var maxDownloads: Int?
+    
     @Option(name: .customLong("since"), help: "Since date")
     private var sinceDateString: String?
     
@@ -34,7 +37,7 @@ struct GetVideoEventsMedia: BlinkCommand {
     
     func run() throws {
         var cancellables = Set<AnyCancellable>()
-        await { exit in
+        `await` { exit in
             let mediaStorage: MediaStorage = defaultMediaStorage(rootURL: rootURL)
             let blinkController = BlinkController(globalOptions: globalOptions)
             let videoEventsForPage = { blinkController.videoEvents(page: $0, since: sinceDate) }
@@ -50,7 +53,16 @@ struct GetVideoEventsMedia: BlinkCommand {
                 .eraseToAnyPublisher()
             let getVideo = { blinkController.getVideo(media: $0) }
             let urlForStoredMedia = { mediaStorage.urlForMedia($0) }
-            return Self.downloadMedia(queryMedia: queryMedia, getVideo: getVideo, urlForStoredMedia: urlForStoredMedia)
+            let downloadMedia = { Self.downloadMedia(getVideo: getVideo, media: $0, mediaURL: $1) }
+            let queryMediaForDownload = {
+                Self
+                    .queryMediaForDownload(queryMedia: queryMedia, urlForStoredMedia: urlForStoredMedia)
+            }
+            return queryMediaForDownload()
+                .flatMap(
+                    maxPublishers: maxDownloads.flatMap { .max($0) } ?? .unlimited,
+                    downloadMedia
+                )
                 .awaitAndTrack(exit: exit, cancellables: &cancellables)
         }
     }
@@ -72,29 +84,37 @@ struct GetVideoEventsMedia: BlinkCommand {
 extension GetVideoEventsMedia {
     
     static func downloadMedia(
-        queryMedia: AnyPublisher<[Media], Error>,
         getVideo: @escaping (String) -> AnyPublisher<VideoResponse, Error>,
+        media: String,
+        mediaURL: URL,
+        fileManager: FileManager = .default
+    ) -> AnyPublisher<URL, Error> {
+        getVideo(media)
+            .tryMap { response -> URL in
+                let mediaDirectoryURL = mediaURL.deletingLastPathComponent()
+                try fileManager.createDirectory(at: mediaDirectoryURL, withIntermediateDirectories: true, attributes: nil)
+                try fileManager.moveItem(at: response.url, to: mediaURL)
+                return mediaURL
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    static func queryMediaForDownload(
+        queryMedia: AnyPublisher<[Media], Error>,
         urlForStoredMedia: @escaping (Media) -> URL,
         fileManager: FileManager = FileManager.default
-    ) -> AnyPublisher<URL, Error> {
+    ) -> AnyPublisher<(media: String, mediaURL: URL), Error> {
         queryMedia
             .flatMap { (media: [Media]) in
                 Publishers.Sequence<[Media], Error>(sequence: media)
             }
-            .flatMap { (media: Media) -> AnyPublisher<URL, Error> in
+            .compactMap { (media: Media) in
                 let mediaURL = urlForStoredMedia(media)
                 assert(mediaURL.isFileURL)
                 guard fileManager.fileExists(atPath: mediaURL.path) == false else {
-                    return Result<URL, Error>.Publisher(mediaURL).eraseToAnyPublisher()
+                    return nil
                 }
-                return getVideo(media.media)
-                    .tryMap { response in
-                        let mediaDirectoryURL = mediaURL.deletingLastPathComponent()
-                        try fileManager.createDirectory(at: mediaDirectoryURL, withIntermediateDirectories: true, attributes: nil)
-                        try fileManager.moveItem(at: response.url, to: mediaURL)
-                        return mediaURL
-                    }
-                    .eraseToAnyPublisher()
+                return (media: media.media, mediaURL: mediaURL)
             }
             .eraseToAnyPublisher()
     }
