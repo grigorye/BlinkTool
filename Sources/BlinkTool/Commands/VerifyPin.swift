93d2d1d4c269769396c505bfdcd4024a77b8eeed
Grigory Entin <grigory.entin@gmail.com>
2022-11-12 11:54:49 +0100

    Tweaked VerifyPin for --password clash with the global option.

--- Sources/BlinkTool/Commands/VerifyPin.swift
+++ Sources/BlinkTool/Commands/VerifyPin.swift
@@ -10,12 +10,9 @@ struct VerifyPin: AsyncParsableCommand {
     @Option(help: "Pin")
     private var pin: String
     
-    @Option(help: "Password")
-    private var password: String
-    
     func run() async throws {
         try await track {
-            try await BlinkAuthenticator(email: globalOptions.email, password: password, reauth: globalOptions.reauth)
+            try await BlinkAuthenticator(email: globalOptions.email, password: globalOptions.password, reauth: globalOptions.reauth)
                 .verifyPin(pin: pin)
         }
     }
