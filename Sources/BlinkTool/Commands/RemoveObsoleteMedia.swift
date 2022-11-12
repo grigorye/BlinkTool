c49a57e9ada9cb4dc3e20ae477027e8b3ac8a55e
Grigory Entin <grigory.entin@gmail.com>
2022-11-12 01:37:11 +0100

    Added RemoveObsoleteMedia command.

---
+++ Sources/BlinkTool/Commands/RemoveObsoleteMedia.swift
@@ -0,0 +1,45 @@
+import ArgumentParser
+import AsyncAlgorithms
+import BlinkKit
+import Foundation
+
+protocol ExistingMediaStorage {
+    func existingMedia() async throws -> [URL]
+}
+
+struct RemoveObsoleteMedia: BlinkCommand {
+    
+    public static let configuration = CommandConfiguration(
+        abstract: "Get media for video events"
+    )
+    
+    @Option(name: .customLong("destination"), help: "Root")
+    private var destinationPath: String?
+    
+    private var rootURL: URL {
+        destinationPath.flatMap { URL(fileURLWithPath: $0) } ?? defaultMediaStorageRootURL()
+    }
+    
+    func run() async throws {
+        let mediaStorage = defaultMediaStorage(rootURL: rootURL)
+        let blinkController = try await blinkController()
+        let videoEventsForPage = { try await blinkController.videoEvents(page: $0, since: .distantPast) }
+        let queryMedia: () async throws -> [Media] = {
+            try await GetVideoEvents.queryMediaForAllPages(videoEventsForPage: videoEventsForPage)
+        }
+        
+        let urlForStoredMedia = { mediaStorage.urlForMedia($0) }
+        async let existingMedia = try await mediaStorage.existingMedia()
+        let remoteMedia = try await queryMedia().map { urlForStoredMedia($0).resolvingSymlinksInPath() }
+        let mediaToRemove = try await existingMedia.filter { !remoteMedia.contains($0) }
+        
+        await track {
+            mediaToRemove.async.map { mediaURL in
+                try! FileManager.default.removeItem(at: mediaURL)
+                return mediaURL
+            }
+        }
+    }
+    
+    @OptionGroup var globalOptions: GlobalOptions
+}
